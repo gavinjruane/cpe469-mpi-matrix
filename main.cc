5a5137cc571b7e8aa65f58cd9d80ddfd92e3712b
Gavin Ruane <gjrswim+github@gmail.com>
2026-03-02 22:31:11 -0800

    [main.cc] Implemented basic Matrix class and sequential multiplication

---
+++ main.cc
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cstdlib>
+#include <random>
+#include <string>
+#include <vector>
+
+typedef std::vector<std::vector<int>> MatrixVector;
+
+class Matrix {
+  public:
+    explicit Matrix(std::size_t size)
+      : size_(size),
+        matrix_(random_matrix(size_)) {}
+
+    std::size_t size() const { return size_; }
+    const MatrixVector &data() const { return matrix_; }
+
+    std::string to_string() const {
+      std::string out = "[\n";
+      for (int r = 0; r < size_; r++) {
+        out += "[";
+        for (int c = 0; c < size_; c++) {
+          if (c <= size_ - 2) {
+            out += std::to_string(matrix_[r][c]) + ", ";
+          } else {
+            out += std::to_string(matrix_[r][c]);
+          }
+        }
+        out += "]\n";
+      }
+      out += "]\n";
+
+      return out;
+    }
+
+    Matrix sequential_multiply(
+      Matrix &m2
+    ) const {
+      Matrix result(this->size());
+
+      for (int r = 0; r < size(); r++) {
+        for (int c = 0; c < size(); c++) {
+          result.matrix_[r][c] = dot_product(size(), matrix_[r], m2.column_at(c));
+        }
+      }
+
+      return result;
+    }
+
+  private:
+    std::size_t size_;
+    std::vector<std::vector<int>> matrix_;
+
+    MatrixVector empty_matrix(std::size_t size) {
+      MatrixVector m(size, std::vector<int>(size, 0));
+
+      return m;
+    }
+
+    std::vector<int> column_at(
+      int column
+    ) const {
+      std::vector<int> colvec(size(), 0);
+
+      for (int i = 0; i < size(); i++) {
+        colvec[i] = matrix_[i][column];
+      }
+
+      return colvec;
+    }
+
+    int dot_product(
+      std::size_t size,
+      std::vector<int> v1,
+      std::vector<int> v2
+    ) const {
+      int result = 0;
+
+      for (int i = 0; i < size; i++) {
+        result += v1[i] * v2[i];
+      }
+
+      return result;
+    }
+
+    MatrixVector random_matrix(std::size_t size) {
+      MatrixVector m(size, std::vector<int>(size, 0));
+
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      std::uniform_int_distribution<int> dist(0, 1000);
+    
+      for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+          m[r][c] = dist(gen);
+        }
+      }
+
+      return m;
+    }
+};
+
+int main () {
+  Matrix matrix1{2};
+  Matrix matrix2{2};
+
+  std::cout << matrix1.to_string();
+  std::cout << matrix2.to_string();
+
+  Matrix result_matrix = matrix1.sequential_multiply(matrix2);
+  std::cout << result_matrix.to_string();
+  
+  return 0;
+}
